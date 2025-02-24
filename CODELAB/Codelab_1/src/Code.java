e7ad98cdd23e38c8466c3501b1d32a6ce2c0538d
KharismaAditya <yenderruaditya@gmail.com>
2025-02-24 12:16:49 +0700

    perubahan 1

--- CODELAB/Codelab_1/src/Code.java
+++ CODELAB/Codelab_1/src/Code.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Code {
     public static void main(String[] args){
-        System.out.println("UPDATE");
+        Scanner input = new Scanner(System.in);
+        String nama;
+        String kelamin;
+        int umur;
+
+        System.out.print("Masukkan nama: ");nama = input.nextLine();
+        System.out.print("Masukkan jenis kelamin (P/L): "); kelamin = input.nextLine();
+        System.out.print("Masukkan umur: ");umur = input.nextInt();
+
+        if(kelamin.equalsIgnoreCase("p")){
+            kelamin = "Perempuan";
+        } else if (kelamin.equalsIgnoreCase("l")) {
+            kelamin = "Laki-laki";
+        }else{
+            System.err.println("KELAMIN TIDAK DITEMUKAM. HARAP MASUKKAN KODE YANG BENAR");
+            kelamin = "ERROR";
+        }
+
+        System.out.println("Data diri: ");
+        System.out.println("Nama          : " + nama);
+        System.out.println("Jenis kelamin : " + kelamin);
+        System.out.println("Umur          : " + umur + " Tahun");
     }
+
 }
--- TASK/tugas/src/code.java
+++ TASK/tugas/src/code.java
@@ -1,2 +1,51 @@
+import java.util.Scanner;
+
 public class code {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Pilih login: ");
+        System.out.println("1. Admin\n2. Mahasiswa");
+        System.out.print("Masukkan pilihan: ");String pilih = input.next();
+        switch (pilih){
+            case "1":
+                admin(input);
+                break;
+            case "2":
+                logmhs(input);
+                break;
+            default:
+                System.err.println("INPUT YANG ANDA MASUKKAN TIDAK VALID");
+        }
+    }
+
+    private static void admin(Scanner input){
+        input.nextLine();
+        System.out.println("Login admin");
+        System.out.print("Masukkan Username: ");String admin = input.nextLine();
+        System.out.print("Masukkan Password: "); String pass = input.nextLine();
+
+        if(admin.equals("Admin200") && pass.equals("Password200")){
+            System.out.println("Login admin berhasil");
+        }else{
+            System.err.println("Login gagal, Username atau password salah!!");
+        }
+    }
+
+    private static void logmhs(Scanner input){
+        input.nextLine();
+        String nb = "Muhammad Kharisma Aditya Putra";
+        String nimb = "202410370110200";
+
+        System.out.println("Login Mahasiswa");
+        System.out.print("Masukkan Nama: "); String nama = input.nextLine();
+        System.out.print("Masukkan NIM: "); String nim = input.nextLine();
+
+        if(nama.equals(nb) && nim.equals(nimb)){
+            System.out.println("Login Mahasiswa berhasil");
+            System.out.println("Nama: " + nama);
+            System.out.println("NIM : " + nim);
+        }else{
+            System.err.println("Login gagal, Nama atau NIM salah!");
+        }
+    }
 }

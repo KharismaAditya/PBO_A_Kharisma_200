27f4abe0106c36b6bfd3fb5b3d1c67a893cd7c1a
KharismaAditya <yenderruaditya@gmail.com>
2025-02-24 12:32:31 +0700

    perubahan ketiga

--- CODELAB/Codelab_1/src/Code.java
+++ CODELAB/Codelab_1/src/Code.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
+import java.time.LocalDateTime;
 
 public class Code {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        LocalDateTime timenow = LocalDateTime.now();
+
         String nama;
         String kelamin;
-        int umur;
+        int tahunlahir;
 
         System.out.print("Masukkan nama: ");nama = input.nextLine();
         System.out.print("Masukkan jenis kelamin (P/L): "); kelamin = input.nextLine();
-        System.out.print("Masukkan umur: ");umur = input.nextInt();
+        System.out.print("Masukkan umur: ");tahunlahir = input.nextInt();
 
         if(kelamin.equalsIgnoreCase("p")){
             kelamin = "Perempuan";
@@ -19,6 +22,9 @@ public class Code {
             System.err.println("KELAMIN TIDAK DITEMUKAM. HARAP MASUKKAN KODE YANG BENAR");
             kelamin = "ERROR";
         }
+
+        int umur = timenow.getYear() - tahunlahir;
+
 
         System.out.println("Data diri: ");
         System.out.println("Nama          : " + nama);

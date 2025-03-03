3cabf2caa25658ee585ec682f71941a7c5157d27
KharismaAditya <yenderruaditya@gmail.com>
2025-03-03 10:00:15 +0700

    Penggunaan Java.math.*

--- TASK/tugas/src/code.java
+++ TASK/tugas/src/code.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
+import java.math.*;
 
 public class code {
-
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         boolean loop = true;
@@ -22,18 +22,19 @@ public class code {
                     System.out.println("\n");
             }
         }
-        System.out.printf("\n");
-        System.out.println("TERIMA KASIH TELAH MENGGUNAKAN PROGRAM INI");
-
     }
 
     private static boolean admin(Scanner input){
         input.nextLine();
+
+        final String cadmin = "Admin200";
+        final String cpass = "Password200";
+
         System.out.println("Login page admin");
         System.out.print("Masukkan Username: ");String admin = input.nextLine();
         System.out.print("Masukkan Password: "); String pass = input.nextLine();
 
-        if(admin.equals("Admin200") && pass.equals("Password200")){
+        if(admin.equals(cadmin) && pass.equals(cpass)){
             System.out.println("Login admin berhasil");
         }else{
             System.err.println("Login gagal, Username atau password salah!!");
@@ -43,20 +44,25 @@ public class code {
 
     private static boolean logmhs(Scanner input){
         input.nextLine();
-        String nb = "Muhammad Kharisma Aditya Putra";
-        String nimb = "202410370110200";
+        final String nb = "Muhammad Kharisma Aditya Putra";
+        final BigInteger nimb = new BigInteger("202410370110200");
+        try {
+            System.out.println("Login Mahasiswa");
+            System.out.print("Masukkan Nama: "); String nama = input.nextLine();
+            System.out.print("Masukkan NIM: "); BigInteger nim = new BigInteger(input.nextLine());
 
-        System.out.println("Login Mahasiswa");
-        System.out.print("Masukkan Nama: "); String nama = input.nextLine();
-        System.out.print("Masukkan NIM: "); String nim = input.nextLine();
-
-        if(nama.equals(nb) && nim.equals(nimb)){
-            System.out.println("Login Mahasiswa berhasil");
-            System.out.println("Nama: " + nama);
-            System.out.println("NIM : " + nim);
-        }else{
-            System.err.println("Login gagal, Nama atau NIM salah!");
+            if(nama.equals(nb) && nim.equals(nimb)){
+                System.out.println("\n");
+                System.out.println("Login Mahasiswa berhasil");
+                System.out.println("Nama: " + nama);
+                System.out.println("NIM : " + nim);
+            }else{
+                System.err.println("Login gagal, Nama atau NIM salah!");
+            }
+        } catch (NumberFormatException e) {
+            System.err.println("ERROE: NIM HARUS BERUPA ANGKA!!");
         }
+
         return false;
     }
 }

65c3310b2c0cdc4245c704b66d1a4242df64a245
KharismaAditya <yenderruaditya@gmail.com>
2025-03-09 01:03:30 +0700

    CODELAB 2

--- CODELAB/CODELAB_2-2/src/BankAccount.java
+++ CODELAB/CODELAB_2-2/src/BankAccount.java
@@ -1,2 +1,36 @@
 public class BankAccount {
+    String accountNumber;
+    String ownerName;
+    double balance;
+
+    void displayinfo(){
+        System.out.println("Nomor Rekening: " + accountNumber);
+        System.out.println("Nama Pemilik: " + ownerName);
+        System.out.println("Saldo: Rp" + balance);
+        System.out.print("\n");
+    }
+
+    void depositinfo(double ammount){
+        balance += ammount;
+        System.out.println(ownerName + " menyetor Rp" + ammount + ". Saldo sekarang: Rp" + balance);
+    }
+
+    String periksa(double ammount,double balance){
+        if(ammount > balance){
+            return "(GAGAL, Saldo tidak mencukupi)";
+        }
+        return  "(Berhasil)";
+    }
+
+    double tariktunai(double ammount, double balance){
+        if(ammount > balance){
+            return balance;
+        }
+        return balance - ammount;
+    }
+
+    void withdrawMoney(double ammount){
+        System.out.println(ownerName + " Menarik RP" + ammount + periksa(ammount, balance) + " Saldo sekarang: Rp" +tariktunai(ammount, balance));
+
+    }
 }
--- CODELAB/CODELAB_2-2/src/Main.java
+++ CODELAB/CODELAB_2-2/src/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+    public static void main(String[] args){
+        BankAccount account1 = new BankAccount();
+        BankAccount account2 = new BankAccount();
+
+        account1.accountNumber = "202410370110200";
+        account1.ownerName = "Kharisma Aditya";
+        account1.balance = 50000;
+
+        account2.accountNumber = "202410370110039";
+        account2.ownerName = "Firdaus";
+        account2.balance = 40000;
+
+        account1.displayinfo();
+        account2.displayinfo();
+
+        System.out.print("\n");
+        account1.depositinfo(20000);
+        account2.depositinfo(10000);
+
+        System.out.print("\n");
+        account1.withdrawMoney(80000);
+        account2.withdrawMoney(30000);
+    }
 }
